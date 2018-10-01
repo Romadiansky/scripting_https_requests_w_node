812cfecbf68f6ba1af088c677c66719ff2b5cea5
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-01 21:44:47 +0000

    lower case happened!

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -1,1 +1,12 @@
 var getHTML = require('./http_functions');
+
+var options = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowerCase (html) {
+  console.log(html.toLowerCase());
+}
+
+getHTML(options, printLowerCase);
--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,1 +1,12 @@
 var getHTML = require('./http_functions');
+
+var options = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+  console.log(html.reverse());
+}
+
+getHTML(options, printReverse);
